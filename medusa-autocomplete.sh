41ca5c5607efee10ef3766ae824e5c0ca831943c
Ahmad Amireh <ahmad@amireh.net>
2018-07-07 18:15:17 +0300

    initial commit, port over from pamm

---
+++ medusa-autocomplete.sh
@@ -0,0 +1,15 @@
+# nevermind the -W flag, it's obtrusive
+#
+# commands="
+#   ansible-galaxy
+#   ansible-playbook
+#   ansible-vault
+#   decrypt-file
+#   encrypt-file
+#   encrypt-string
+#   go
+# "
+#
+# complete -W "${commands}" -o bashdefault -o default 'medusa'
+
+complete -W "${commands}" -o bashdefault -o default 'medusa'
